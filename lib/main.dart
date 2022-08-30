0303cf49f447cf08ad52a22efdf01443401f06e8
unknown <onlyjoid2022@gmail.com>
2022-08-30 14:37:55 +0700

    humnoi

---
+++ lib/main.dart
@@ -0,0 +1,141 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<MyApp> {
+  String result = "0", expression = "";
+
+  buttonPressed(String value) {
+    print(value);
+
+    setState(() {
+      if (value == "CLEAR") {
+        result = "0";
+      } else if (value == ".") {
+        if (result.contains(".")) {
+          return;
+        } else {
+          result = result + value;
+        }
+      } 
+       else if (value == "=") {
+        expression = result.replaceAll("X", "*");
+        Parser p = Parser();
+        Expression exp = p.parse(expression);
+        ContextModel cm = ContextModel();
+        dynamic calculate = exp.evaluate(EvaluationType.REAL, cm);
+
+        result = "$calculate";
+      } else {
+        if (result == "0") {
+          result = value;
+        } else {
+          result = result + value;
+        }
+      }
+    });
+  }
+
+  Widget myButton(String buttonLabel) {
+    return Padding(
+      padding: EdgeInsets.all(15),
+      child: OutlinedButton(
+        
+        onPressed: () => buttonPressed(buttonLabel),
+        child: Text(
+          buttonLabel,
+          style: TextStyle(
+            fontSize: 20,
+            color: Color.fromARGB(255, 33, 17, 248),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(155, 39, 19, 214),
+          title: Text("Calculator"),
+        ),
+        body: SafeArea(
+          child: Column(
+            children: [
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 12, vertical: 24),
+                alignment: Alignment.centerRight,
+                child: Text(
+                  result,
+                  style: TextStyle(fontSize: 70, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Expanded(
+                child: Divider(),
+              ),
+              Column(
+                children: [
+                  
+                  Row(
+                    children: [
+                      myButton("7"),
+                      myButton("8"),
+                      myButton("9"),
+                      myButton("/"),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      myButton("4"),
+                      myButton("5"),
+                      myButton("6"),
+                      myButton("X"),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      myButton("1"),
+                      myButton("2"),
+                      myButton("3"),
+                      myButton("-"),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      myButton("."),
+                      myButton("0"),
+                      myButton("00"),
+                      myButton("+"),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      myButton("CLEAR"),
+                      myButton("="),
+                      
+                    ],
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
